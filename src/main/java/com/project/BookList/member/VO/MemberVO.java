416f4750a5df163c6f129d229df47e9dd1b56a54
dasol <dspark960927@gmail.co>
2023-05-14 01:49:38 +0900

    2023-05-14 01:49

--- src/main/java/com/project/BookList/member/VO/MemberVO.java
+++ src/main/java/com/project/BookList/member/VO/MemberVO.java
@@ -4,14 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberVO {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String M_ID;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    //@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String M_PW;
+
+    @NotBlank(message = "이름을 입력해주세요.")
     private String M_NAME;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String M_NICKNAME;
-    private String M_GENDER;
+
+    @NotBlank(message = "생년월일를 입력해주세요.")
     private String M_BDATE;
 }
--- src/main/java/com/project/BookList/member/controller/MemberController.java
+++ src/main/java/com/project/BookList/member/controller/MemberController.java
@@ -4,10 +4,14 @@ import com.project.BookList.member.VO.MemberVO;
 import com.project.BookList.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class MemberController {
@@ -19,22 +23,43 @@ public class MemberController {
         return "login";
     }
 
-
-
-    @RequestMapping("/join")
+    @GetMapping("/join")
     public String MemberInsertForm(@ModelAttribute MemberVO member){
 
         return "join";
     }
 
-    @RequestMapping("/insertProc")
+
+    @PostMapping("/join")
     public String MemberInsertProc(@ModelAttribute MemberVO member, HttpServletRequest request){
+
         System.out.println(member);
 
         memberService.memberInsert(member);
 
-        return "join";
+        return "joinAccess";
     }
+
+
+    /*
+    @PostMapping("/join")
+    public String MemberInsertProc(@Valid MemberVO member, BindingResult result, HttpServletRequest request){
+
+
+
+        if (result.hasErrors()){
+            System.out.println("!!");
+            return "join";
+        }
+
+        System.out.println(member);
+
+        memberService.memberInsert(member);
+
+        return "joinAccess";
+    }
+
+     */
 
 
     @RequestMapping("/findId")
